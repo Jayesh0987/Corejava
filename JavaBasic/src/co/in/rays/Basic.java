9973f0da955dc0ed2a92c35ca973a66ec85baabf
Jayesh09876 <jayeshmishra1432@gmail.com>
2023-07-31 12:34:09 +0530

    save

---
+++ JavaBasic/src/co/in/rays/Basic.java
@@ -0,0 +1,9 @@
+package co.in.rays;
+
+public class Basic {
+	public static void main(String[] args) {
+		
+		System.out.print("brajesh");
+		System.out.print("hello");
+	}
+}
---
+++ JavaBasic/src/co/in/rays/loop.java
@@ -0,0 +1,16 @@
+package co.in.rays;
+
+public class loop {
+	
+	
+	public static void main(String[] args) {
+		int money =  100;
+		if (money > 110) {
+			System.out.println("i cant buy pizza");
+			
+		}else {
+			System.out.println("i can buy pizza");
+		}
+	}
+
+}
